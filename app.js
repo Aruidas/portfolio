60ac3577d33d0917ff1c6b269d293b218d720888
Audrius <86117093+Aruidas@users.noreply.github.com>
2021-08-16 17:23:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 translateText(); // Apply initial translation once the page is loaded
 
 
+const sections = document.querySelectorAll('.anchor');
+const listItems = document.querySelectorAll('#nav-menu li');
+window.addEventListener('scroll', () => {
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if(pageYOffset >= sectionTop - 300) { // -X: offset, to highlight links earlier
+            currentSection = section.getAttribute('id');
+        }
+    })
+
+    listItems.forEach(li => {
+        li.classList.remove('current');
+        if(li.firstElementChild.getAttribute('href') == '#'+currentSection) {
+            li.classList.add('current');
+        }
+    })
+})
+
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+function closeMobileMenu() {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('open');
+}
 function toggleMenu() {
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('nav-menu');
-    
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('open');
 }
